2f927ee775e907e8914ed2d48e3f531b05044e3d
Alberto Lima <alberto.btlima@outlook.com>
2025-07-21 11:13:58 -0300

    pwa config

--- 01-react-transforme-sua-aplicacao-web-em-uma-pwa/vite.config.js
+++ 01-react-transforme-sua-aplicacao-web-em-uma-pwa/vite.config.js
@@ -6,6 +6,34 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        id: "/",
+        name: "Jornada Milhas",
+        short_name: "Jornada",
+        theme_color: "#5a189a",
+        background_color: "#09001a",
+        display: "standalone",
+        description: "Site do Jornada Milhas",
+        orientation: "portrait",
+        start_url: "/",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: { enabled: true },
+    }),
   ],
 });
