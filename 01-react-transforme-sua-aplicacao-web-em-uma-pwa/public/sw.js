eb20e8a01ff98ea531840744fe43129adb92d83f
Alberto Lima <alberto.btlima@outlook.com>
2025-08-03 13:13:28 -0300

    add: sw.js

---
+++ 01-react-transforme-sua-aplicacao-web-em-uma-pwa/public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "meu_cache";
+
+self.addEventListener("install", (event) => {
+  console.log("Instalando o service worker");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["/index.html"]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(`Baixando ${event.request.url}`);
+  event.respondWith(cachePrimeiro(event.request));
+});
+
+const cachePrimeiro = async (request) => {
+  const respostaDoCache = await caches.match(request);
+
+  if (respostaDoCache) {
+    return respostaDoCache;
+  }
+
+  const respostaRede = await fetch(request);
+  atualizaCache(request, respostaRede.clone());
+
+  return respostaRede;
+};
+
+const atualizaCache = async (request, response) => {
+  const cache = await caches.open(CACHE_NAME);
+  await cache.put(request, response);
+};
