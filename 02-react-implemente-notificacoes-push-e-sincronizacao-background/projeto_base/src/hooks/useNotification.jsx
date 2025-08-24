7966b92e0f319986a7cb918b6fc66270cdc6392d
Alberto Lima <alberto.btlima@outlook.com>
2025-08-24 11:31:25 -0300

    up

--- 02-react-implemente-notificacoes-push-e-sincronizacao-background/projeto_base/src/hooks/useNotification.jsx
+++ 02-react-implemente-notificacoes-push-e-sincronizacao-background/projeto_base/src/hooks/useNotification.jsx
@@ -9,6 +9,14 @@ const useNotification = () => {
     }
   };
 
+  const sendNotification = (title, options) => {
+    if (Notification.permission === "granted") {
+      new Notification(title, options);
+    } else "Notificações não permitidas";
+  };
+
+  const sentTestNotification = () {}
+
   useEffect(() => {
     requestNotificationPermission();
   });
