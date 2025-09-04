c303f37f22960b2012b7958661bfd24d9ad5649c
Alberto Lima <alberto.btlima@outlook.com>
2025-09-04 15:38:54 -0300

    import

---
+++ 02-react-implemente-notificacoes-push-e-sincronizacao-background/projeto_base/public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5oxO72RjKGDDQHFSyHF_N0i2A0Izb0i0",
+  authDomain: "jornada-milhas-9715c.firebaseapp.com",
+  projectId: "jornada-milhas-9715c",
+  storageBucket: "jornada-milhas-9715c.firebasestorage.app",
+  messagingSenderId: "423766070192",
+  appId: "1:423766070192:web:7465f37525e466443c3c75",
+};
